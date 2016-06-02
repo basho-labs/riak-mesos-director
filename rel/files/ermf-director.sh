883e4e8546f672eb3f5036705d34c4731f38ec83
Drew Kerrigan <dr.kerrigan@gmail.com>
2016-06-02 13:40:35 -0400

    fixing nodename

--- rel/files/ermf-director.sh
+++ rel/files/ermf-director.sh
@@ -7,6 +7,9 @@ main() {
         export HOME=`eval echo "~$WHOAMI"`
     fi
 
+    NODENAME="$DIRECTOR_FRAMEWORK-$DIRECTOR_CLUSTER-director@127.0.0.1"
+    sed -i "s,nodename = riak_mesos_director@127.0.0.1,nodename = $NODENAME,g" riak_mesos_director/etc/director.conf
+
     echo "Starting director..."
     riak_mesos_director/bin/director console -noinput #-no_epmd
 }
